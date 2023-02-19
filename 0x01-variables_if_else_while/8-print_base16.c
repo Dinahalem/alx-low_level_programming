af69b7848154e960a0e8d3041bcd4f484fd50a71
dina2022 <dina_abdelhalim2018@yahoo.com>
2023-02-18 23:30:29 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always (Success)
+ *
+ */
+
+int main(void)
+{
+int i;
+for (i = '0'; i <= 'F'; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
