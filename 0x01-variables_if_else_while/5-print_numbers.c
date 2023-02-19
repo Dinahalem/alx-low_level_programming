77eb401b80bb529d07e276085c5926177f77ba0c
dina2022 <dina_abdelhalim2018@yahoo.com>
2023-02-18 23:04:45 -0800

    update 5-prints

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,16 +3,19 @@
 #include <stdio.h>
 
 /**
- * main - Prints a text according number
+ * main - Entry point
  * Return: Always (Success)
  *
  */
 
 int main(void)
 {
+
 int i;
 for (i = 0; i <= 10; i++)
 printf("%d", i);
 printf("\n");
 return (0);
+
 }
+
